9716938cdd6c52aa913172534126c27e2237eed5
Guillem Poy <guplem@gmail.com>
2024-06-01 16:43:49 +0200

    ACtion

--- pages/game_page.py
+++ pages/game_page.py
@@ -1,12 +1,10 @@
-import streamlit as st
-
 from game.game_manager import GameManager
-from widgets.game_setup import game_setup
-from widgets.game_situation import game_situation
+from widgets.game_setup_widget import game_setup
+from widgets.game_situation import game_state
 
 # If the game Master has the setup done
 if not GameManager().is_game_started():
     game_setup()
 
 else:
-    game_situation()
+    game_state()
--- widgets/game_situation.py
+++ widgets/game_situation.py
@@ -3,7 +3,7 @@ import streamlit as st
 from game.game_manager import GameManager
 
 
-def game_situation():
+def game_state():
 
     st.title(GameManager().get_current_state().location)
     st.write(GameManager().get_current_state().narrative)
@@ -11,3 +11,9 @@ def game_situation():
     st.title("Characters in this location:")
     for character in GameManager().get_characters_in_current_location():
         st.write(character.name)
+
+    action = ""
+    st.text_input("Action", value=action, key="action")
+    def on_submit():
+        GameManager().perform_action(st.session_state.action)
+    st.button("Perform Action", on_click=on_submit)
