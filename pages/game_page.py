a6c5242ab8e9c55444639bcd0c7fc3e9cfb410e4
Guillem Poy <guplem@gmail.com>
2024-06-01 20:24:03 +0200

    About

--- pages/game_page.py
+++ pages/game_page.py
@@ -1,6 +1,23 @@
+import streamlit as st
+
 from game.game_manager import GameManager
 from widgets.game_setup_widget import game_setup
 from widgets.game_situation import game_state
+
+
+st.set_page_config(
+    page_title="GPT-Role",
+    page_icon="🎲",
+    menu_items={"About":
+       "## Authors\n"
+       "- [Guillem Poy](https://github.com/guplem)\n"
+       "- [Sergi Romero](https://github.com/sergiromero)\n"
+       "- [Oriol Hinojo](https://github.com/hinox13)\n"
+       "## Repository\n"
+       "[Link to the repository](https://github.com/guplem/GPT-Role)\n"
+       "\n"
+    }
+)
 
 # If the game Master has the setup done
 if not GameManager().is_game_started():
