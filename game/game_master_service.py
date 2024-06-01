a868c921ca02f648c3a10c1c71f09bdbf40605f9
Sergi Romero <sergi.romero@innovamat.com>
2024-06-01 15:10:59 +0200

    Return types and static methods

--- game/game_master_service.py
+++ game/game_master_service.py
@@ -1,11 +1,31 @@
-from openai import OpenAI
+import random
 
-from game.game_master_prompt import GameMasterPrompt
+from openai import OpenAI
+from openai.types.chat import ChatCompletion
 
 from data.data_service import DataService
+from game.game_master_prompt import GameMasterPrompt
 
 
 class GameMasterService:
+    TOOLS = [
+        {"type": "function",
+         "function": {
+             "name": "conflict",
+             "description": GameMasterPrompt.CONFLICT
+         }},
+        {"type": "function",
+         "function": {
+             "name": "role_playing",
+             "description": GameMasterPrompt.ROLE_PLAYING
+         }},
+        {"type": "function",
+         "function": {
+             "name": "story_telling",
+             "description": GameMasterPrompt.STORY_TELLING
+         }},
+    ]
+
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -15,24 +35,6 @@ class GameMasterService:
 
     def call_llm(self, prompt) -> None:
         # This is effectively telling ChatGPT what we're going to use its JSON output for.
-        tools = [
-            {"type": "function",
-             "function": {
-                 "name": "conflict",
-                 "description": GameMasterPrompt.CONFLICT
-             }},
-            {"type": "function",
-             "function": {
-                 "name": "role_playing",
-                 "description": GameMasterPrompt.ROLE_PLAYING
-             }},
-            {"type": "function",
-             "function": {
-                 "name": "story_telling",
-                 "description": GameMasterPrompt.STORY_TELLING
-             }},
-        ]
-
         client = OpenAI()
         # The request to the ChatGPT API.
         response = client.chat.completions.create(
@@ -42,36 +44,37 @@ class GameMasterService:
                  "content": f"{GameMasterPrompt.DECIDE_ACTION_BOT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
                 {"role": "user", "content": f"{prompt}"}
             ],
-            tools=tools,
+            tools=self.TOOLS,
             tool_choice="required"
         )
 
-        import random
-        def conflict():
-            dice = random.randint(1, 20)
-            messages = [
-                {"role": "system",
-                 "content": f"{GameMasterPrompt.GAME_MASTER_CONFLICT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
-                {"role": "user",
-                 "content": f"{prompt}\nI rolled a d20 dice and I got this number: {dice} \nHow does the story continue?"}]
-            response = client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-            )
-            return response, dice
-
-        def role_playing():
-            dice = random.randint(1, 20)
-            print("Hosla roleplaying", dice)
-            return response
-
-        def story_telling():
-            dice = random.randint(1, 20)
-            print("storypoling", dice)
-            return response
-
-        response2, dice = globals().get(response.choices[0].message.tool_calls[0].function.name)()
+        response2, dice = getattr(self, response.choices[0].message.tool_calls[0].function.name)(prompt)
         return response2.choices[0].message.content
 
     def is_game_started(self) -> bool:
         return DataService().is_game_started()
+
+    @staticmethod
+    def conflict(prompt) -> tuple[ChatCompletion, int]:
+        dice = random.randint(1, 20)
+        client = OpenAI()
+        messages = [
+            {"role": "system",
+             "content": f"{GameMasterPrompt.GAME_MASTER_CONFLICT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
+            {"role": "user",
+             "content": f"{prompt}\nI rolled a d20 dice and I got this number: {dice} \nHow does the story continue?"}]
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+        )
+        return response, dice
+
+    @staticmethod
+    def role_playing(prompt) -> tuple[ChatCompletion, int]:
+        print("Hosla roleplaying")
+        return "", 0
+
+    @staticmethod
+    def story_telling(prompt) -> tuple[ChatCompletion, int]:
+        print("storypoling")
+        return "", 0
