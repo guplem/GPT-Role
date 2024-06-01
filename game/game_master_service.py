17e06a84732bcbe9fe5c60650920050b2aa9d65e
Sergi Romero <sergi.romero@innovamat.com>
2024-06-01 18:03:02 +0200

    Initialize game with personalized definition

--- game/game_master_service.py
+++ game/game_master_service.py
@@ -3,7 +3,6 @@ import random
 from openai import OpenAI
 from openai.types.chat import ChatCompletion
 
-from data.data_service import DataService
 from game.game_master_prompt import GameMasterPrompt
 from models.character import Character
 from models.game_definition import GameDefinition
@@ -30,6 +29,7 @@ class GameMasterService:
          }},
     ]
 
+    game_definition: GameDefinition
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -37,18 +37,17 @@ class GameMasterService:
             cls._instance = super(GameMasterService, cls).__new__(cls)
         return cls._instance
 
-    def start_game(self, game_definition:GameDefinition) -> GameMasterResponse:
-        # TODO: Implement the start_game method
+    def start_game(self, game_definition: GameDefinition) -> GameMasterResponse:
+        self.game_definition = game_definition
         return GameMasterResponse("Welcome Message", [], GameState("Game Started", "start"))
 
-    def perform_action(self, action:str, relevant_characters:[Character], state:GameState, game_definition:GameDefinition, summaries:[str]) -> GameMasterResponse:
-
+    def perform_action(self, action: str, relevant_characters: [Character], state: GameState,
+                       game_definition: GameDefinition, summaries: [str]) -> GameMasterResponse:
         result = self.call_llm(action, "\n".join(summaries))
 
         return GameMasterResponse(result, [], GameState(result, "Action"))
 
-
-    def call_llm(self, prompt:str, summary:str) -> str:
+    def call_llm(self, prompt: str, summary: str) -> str:
         # This is effectively telling ChatGPT what we're going to use its JSON output for.
         client = OpenAI()
         # The request to the ChatGPT API.
@@ -56,7 +55,12 @@ class GameMasterService:
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system",
-                 "content": f"{GameMasterPrompt.DECIDE_ACTION_BOT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
+                 "content": f"{GameMasterPrompt.DECIDE_ACTION_BOT_ROLE} {GameMasterPrompt.GAME_MECHANICS}"
+                            f"{GameMasterPrompt.theme(self.game_definition.theme())}"
+                            f"{GameMasterPrompt.year(self.game_definition.year())}"
+                            f"{GameMasterPrompt.objectives(self.game_definition.objectives())}"
+                            f"{GameMasterPrompt.additional_info(self.game_definition.additional_info())}"
+                            f"{GameMasterPrompt.character_definition(self.game_definition.character_definition())}."},
                 {"role": "user", "content": f"{prompt}"}
             ],
             tools=self.TOOLS,
@@ -66,16 +70,22 @@ class GameMasterService:
         response2 = getattr(self, response.choices[0].message.tool_calls[0].function.name)(prompt, summary)
         return response2.choices[0].message.content
 
-    @staticmethod
-    def conflict(prompt:str, summary:str) -> ChatCompletion:
+    def conflict(self, prompt: str, summary: str) -> ChatCompletion:
         dice = random.randint(1, 20)
         print("[GAME_MASTER_SERVICE] Executing conflict", dice)
         client = OpenAI()
         messages = [
             {"role": "system",
-             "content": f"{GameMasterPrompt.GAME_MASTER_CONFLICT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
+             "content": f"{GameMasterPrompt.GAME_MASTER_CONFLICT_ROLE} "
+                        f"{GameMasterPrompt.GAME_MECHANICS}"
+                        f"{GameMasterPrompt.theme(self.game_definition.theme())}"
+                        f"{GameMasterPrompt.year(self.game_definition.year())}"
+                        f"{GameMasterPrompt.objectives(self.game_definition.objectives())}"
+                        f"{GameMasterPrompt.additional_info(self.game_definition.additional_info())}"
+                        f"{GameMasterPrompt.character_definition(self.game_definition.character_definition())}."
+             },
             {"role": "assistant",
-            "content": f"In this world, the things that have happened before are:\n{summary}"},
+             "content": f"In this world, the things that have happened before are:\n{summary}"},
             {"role": "user",
              "content": f"{prompt}\nI rolled a d20 dice and I got this number: {dice} \nUsing less than 3 sentences, how does the story continue?"}]
         response = client.chat.completions.create(
@@ -84,13 +94,19 @@ class GameMasterService:
         )
         return response
 
-    @staticmethod
-    def role_playing(prompt:str, summary:str) -> ChatCompletion:
+    def role_playing(self, prompt: str, summary: str) -> ChatCompletion:
         print("[GAME_MASTER_SERVICE] Executing role play")
         client = OpenAI()
         messages = [
             {"role": "system",
-             "content": f"{GameMasterPrompt.GAME_MASTER_CONFLICT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
+             "content": f"{GameMasterPrompt.GAME_MASTER_ROLE_PLAYING_ROLE}"
+                        f"{GameMasterPrompt.GAME_MECHANICS}"
+                        f"{GameMasterPrompt.theme(self.game_definition.theme())}"
+                        f"{GameMasterPrompt.year(self.game_definition.year())}"
+                        f"{GameMasterPrompt.objectives(self.game_definition.objectives())}"
+                        f"{GameMasterPrompt.additional_info(self.game_definition.additional_info())}"
+                        f"{GameMasterPrompt.character_definition(self.game_definition.character_definition())}."
+             },
             {"role": "assistant",
              "content": f"In this world, the things that have happened before are:\n{summary}"},
             {"role": "user",
@@ -101,13 +117,20 @@ class GameMasterService:
         )
         return response
 
-    @staticmethod
-    def story_telling(prompt:str, summary:str) -> ChatCompletion:
+    def story_telling(self, prompt: str, summary: str) -> ChatCompletion:
         print("[GAME_MASTER_SERVICE] Executing story telling")
         client = OpenAI()
         messages = [
-            {"role": "system",
-             "content": f"{GameMasterPrompt.GAME_MASTER_CONFLICT_ROLE} {GameMasterPrompt.GAME_MECHANICS}\n{GameMasterPrompt.WORLD_INFO}. {GameMasterPrompt.CHARACTER_INFO}."},
+            {
+                "role": "system",
+                "content": f"{GameMasterPrompt.GAME_MASTER_STORY_TELLING_ROLE}"
+                           f"{GameMasterPrompt.GAME_MECHANICS}"
+                           f"{GameMasterPrompt.theme(self.game_definition.theme())}"
+                           f"{GameMasterPrompt.year(self.game_definition.year())}"
+                           f"{GameMasterPrompt.objectives(self.game_definition.objectives())}"
+                           f"{GameMasterPrompt.additional_info(self.game_definition.additional_info())}"
+                           f"{GameMasterPrompt.character_definition(self.game_definition.character_definition())}."
+            },
             {"role": "assistant",
              "content": f"In this world, the things that have happened before are:\n{summary}"},
             {"role": "user",
--- models/game_definition.py
+++ models/game_definition.py
@@ -1,15 +1,31 @@
 class GameDefinition:
     def __init__(self,  character_definition, year, theme, objective, additional_info):
-        self.characterDefinition = character_definition
-        self.year = year
-        self.theme = theme
-        self.objective = objective
-        self.additionalInfo = additional_info
+        self._characterDefinition = character_definition
+        self._year = year
+        self._theme = theme
+        self._objective = objective
+        self._additionalInfo = additional_info
 
     def __str__(self):
         return f"""
-        Character definition: {self.characterDefinition}
-        \nYear: {self.year}
-        \nTheme: {self.theme}
-        \nObjective: {self.objective}
-        \nAdditionalInfo: {self.additionalInfo}"""
+        Character definition: {self._characterDefinition}
+        Year: {self._year}
+        Theme: {self._theme}
+        Objective: {self._objective}
+        AdditionalInfo: {self._additionalInfo}
+        """
+
+    def character_definition(self) -> str:
+        return self._characterDefinition
+
+    def objectives(self) -> str:
+        return self._objective
+
+    def theme(self) -> str:
+        return self._theme
+
+    def year(self) -> str:
+        return self._year
+
+    def additional_info(self) -> str:
+        return self._additionalInfo
