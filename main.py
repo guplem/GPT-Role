433a204c56ba8d13971c686a2b22a282d8dfae11
Sergi Romero <sergi.romero@innovamat.com>
2024-06-01 11:56:27 +0200

    Load env when starting application

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import streamlit as st
+from dotenv import load_dotenv
+
 from game.game_manager import GameManager
+
+load_dotenv()
 
 # Instantiate GameManager
 if GameManager.get_instance() is None:
