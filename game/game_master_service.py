96425f5542eb8a6365461a0ade0c51a68d2c0f0f
Hinox13 <oriol.hinojo@gmail.com>
2024-06-01 19:48:48 +0200

    update of characters

--- game/game_master_service.py
+++ game/game_master_service.py
@@ -174,14 +174,15 @@ class GameMasterService:
         messages = [
             {
                 "role": "system",
-                "content": "You are bot in charge of updating a list of dictionaries with new information. You always receive a list of dictionaries with characters and you have to update them based on new information. You can only answer with the list of characters. It is possible that some parts of the json need to be updated while keeping meaningful information. If it appears a character that is not in the list it needs to be created considering the following attributes: {name:str, description:str, location:str, inventory:[str]}"
+                "content": "You are bot in charge of updating a list of dictionaries with new information. You always receive a list of dictionaries with characters and you have to update them based on new information. You can only answer with the list of characters. It is possible that some parts of the json need to be updated while keeping meaningful information. If it appears a character that is not in the list it needs to be created considering the following attributes: {name:str, description:str, location:str, inventory:[str]}. The character can be created only if the name, description and location are explicitly said on the text."
             },
             {"role": "user",
-                "content": f"This is my list of characters: {characters}\nThis is the new information: {answer}"},
+                "content": f"This is my list of characters: {characters}\nThis is the new information: {answer}."},
         ]
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=messages,
         )
+        print(response.choices[0].message.content)
         return [Character(**character) for character in eval(response.choices[0].message.content)]
 # Path: models/game_master_response.py
