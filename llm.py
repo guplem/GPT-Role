f0ca74892ca55ea878ee424e1795fc98535c537b
Hinox13 <oriol.hinojo@gmail.com>
2024-06-01 14:30:19 +0200

    added function calling chatjepeto

--- llm.py
+++ llm.py
@@ -1,13 +1,15 @@
 import openai
 from openai import OpenAI
 
-def call(context, prompt, api_key):
+def call(world_info, summaries, context, prompt, api_key):
     client = OpenAI(api_key=api_key)
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"You are a Dungeon Master, {context}"},
-            {"role": "user", "content": prompt}
+            {"role": "system", "content": f"You are a Dungeon Master of a role game about {world_info}.\n The summary of the story is the following: {summaries}."},
+            {"role": "assistant", "content": "I am a player and I am ready to play"},
+            {"role": "user", "content": "Now it's my turn and {prompt}"}
+            
         ]
     )
     return completion.choices[0].message.content
