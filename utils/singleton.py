9fd9935bcacf29818e81e841df8db9652c295f1d
Guillem Poy <guplem@gmail.com>
2024-06-01 22:44:58 +0200

    Update singleton.py

--- utils/singleton.py
+++ utils/singleton.py
@@ -1,11 +1,22 @@
+import streamlit as st
+
 # Extracted from https://stackoverflow.com/questions/6760685/what-is-the-best-way-of-implementing-singleton-in-python#:~:text=Method%203%3A%20A%20metaclass
 
 class Singleton(type):
-    _instances = {}
+    instances = {}
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+        obj = st.session_state.get(cls, None)
+        if obj is None:
+            st.session_state[cls] = cls
+
+        instances = []
+        if obj is not None:
+            instances = obj.instances
+
+        if cls not in instances:
+            st.session_state[cls].instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+
+        return st.session_state[cls].instances[cls]
 
 
 # USAGE:
